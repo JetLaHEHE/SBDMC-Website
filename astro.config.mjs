dd9014d2e957fb56dbfb26978015f780dc6ac9a5
jet <jetagatep@gmail.com>
2026-06-01 21:37:37 +0800

    Update site URL to sbdmc.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://sbdmc-refined.netlify.app",
+  site: "https://sbdmc.netlify.app",
 });
